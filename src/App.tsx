cd63591b442b6a0b46bb3484d0ae722f3ed07081
toyoba78 <rosnitoyaba@gmail.com>
2026-09-16 17:55:53 +0600

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 
 const technologiesFetch = async(): Promise<Itechnologies[]>=> {
-  const res = await fetch("/data.json");
+ const res = await fetch(`${import.meta.env.BASE_URL}data.json`);
   const data = await res.json();
   return data;
 };
