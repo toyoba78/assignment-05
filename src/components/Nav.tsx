aca8f9db1c31f148d848909c1026d989b3b5638b
toyoba78 <rosnitoyaba@gmail.com>
2026-09-16 10:53:46 +0600

    add humburger navber

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,70 @@
-import logo from "../assets/logo-text.png"; 
- 
-const Nav = () => { 
-  return ( 
-    <nav className="flex justify-between container mr-2"> 
-      <img src={logo} alt="Logo" /> 
- 
-      <ul className="flex gap-4 "> 
-        <li>Home</li> 
-        <li>Technologies</li> 
-        <li>Projects</li> 
-        <li>About</li> 
-        <li>Contact</li> 
-      </ul> 
-      <div className="">
-        <button>Sign In</button>
-      <button className="btn-warning px-3">Sign Up</button>
+import logo from "../assets/logo-text.png";
+import hamburger from "../assets/hamburger.png";
+
+
+const Nav = () => {
+  return (
+    <nav className="sticky top-0 z-50">
+
+     
+      <div className="mx-auto hidden h-16 max-w-7xl items-center justify-between px-6 md:flex">
+
+        {/* logo */}
+        <div>
+          <img src={logo}  alt="Dev Stack"  className="h-8 w-auto"  />
+        </div>
+
+        
+        <div className="flex items-center gap-7">
+          <a href="#">Home</a>
+          <a href="#">Technologies</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+
+        {/* button */}
+        <div className="flex items-center gap-4">
+          <button>Sign In</button>
+
+          <button className="rounded-full bg-fuchsia-500 px-4 py-2 text-white">
+            Sign Up
+          </button>
+        </div>
+
       </div>
-    </nav> 
-    
-  ); 
-}; 
- 
-export default Nav; 
+
+
+      {/* Mobile  er jonno*/}
+
+      <div className="mx-auto grid h-16 max-w-7xl grid-cols-3 items-center px-4 md:hidden">
+
+        
+        <div className="justify-self-start">
+          <img  src={hamburger}  alt="Menu"  className="h-6 w-6"  />
+
+        </div>
+
+       
+        <div className="justify-self-center">
+          <img  src={logo}  alt="Dev Stack"  className="h-8 w-auto"  />
+        </div>
+
+        
+        <div className="flex items-center gap-2 justify-self-end">
+          <button className="text-xs">
+            Sign In
+          </button>
+
+          <button className="rounded-full bg-fuchsia-600 px-3 py-1.5 text-xs text-white">
+            Sign Up
+          </button>
+        </div>
+
+      </div>
+
+    </nav>
+  );
+};
+
+export default Nav;
